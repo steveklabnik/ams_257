ba6e0dd7760b39bdb26d6f8188f138cd71f6910f
Steve Klabnik <steve@steveklabnik.com>
2013-03-28 15:09:08 -0700

    dummy rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ams257::Application.config.secret_token = '2dc26ca0301ed21e114247405d080857e159091f351837ba20d0d354b0cc1a5ba6a269f5400924d83c0270b4ce5671d7ae331cbe900a39834883d57ef585ff9d'
